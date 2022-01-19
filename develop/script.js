ac7a8190d10a103aae6080ea4d8a92ded61585f2
Humma Noshine <Hummanoshine@gmail.com>
2022-01-19 17:57:58 +0000

    some updates

--- develop/script.js
+++ develop/script.js
@@ -7,33 +7,13 @@ var times = ["9 am", "10am"];
 var myTEXT = document.querySelector("#myText");
 var saveBtn = document.querySelector(".saveBtn");
 var hour9 = document.querySelector("#hr9");
-renderLastRegistered();
-
-function renderLastRegistered() {
-  var texting = localStorage.getItem("myText");
-
-  if (!texting) {
-    return;
-  }
-  myTEXT.textContent = texting;
-}
 
 saveBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
   console.log("button clicked");
-  var text = document.querySelector("#myText").value;
-
-  localStorage.setItem("myText", text);
-  renderLastRegistered();
-  console.log(text);
+  var texting = document.querySelector("#myText").value;
+  localStorage.setItem("myText", myTEXT);
+  var txt = localStorage.getItem("myText");
+  myTEXT.textContent = txt;
 });
-
-// $("#9 #myText").val(localStorage.getItem("9"));
-
-// $("#save").on("click", function () {
-//   var text = $("#myText").val();
-//   console.log("button cilicked");
-//   // localStorage.setItem(text);
-//   localStorage.setItem("myText", text);
-// });
